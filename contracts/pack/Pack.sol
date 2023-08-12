cd748732f89f9a9a78f2adda9dbbef5bd99b1942
Kien Ngo <kenseverus@gmail.com>
2023-08-11 20:35:11 -0400

    Fix spacing

--- contracts/pack/Pack.sol
+++ contracts/pack/Pack.sol
@@ -352,7 +352,7 @@ contract Pack is
 
         (Token[] memory _token, ) = getPackContents(_packId);
         bool[] memory _isUpdated = new bool[](totalRewardKinds);
-	    for (uint256 i; i < numOfRewardUnitsToDistribute; ) {
+        for (uint256 i; i < numOfRewardUnitsToDistribute; ) {
             uint256 randomVal = uint256(keccak256(abi.encode(random, i)));
             uint256 target = randomVal % totalRewardUnits;
             uint256 step;
@@ -396,7 +396,7 @@ contract Pack is
         contents = new Token[](total);
         perUnitAmounts = new uint256[](total);
 
-	    for (uint256 i; i < total; ) {
+        for (uint256 i; i < total; ) {
             contents[i] = getTokenOfBundle(_packId, i);
             unchecked { ++i; }
         }
